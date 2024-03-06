0803d59589cb5d495c17a37db4a061d4720086a9
yyyejinnn <lyjin2007@gmail.com>
2024-03-06 15:46:36 +0900

    init: set wss

--- src/server.js
+++ src/server.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import http from 'http';
+import ws from 'ws';
 
 const app = express();
 
@@ -6,8 +8,17 @@ app.set('view engine', 'pug');
 app.set('views', __dirname + '/views');
 app.use('/public', express.static(__dirname + '/public'));
 
-app.get('/', (req, res) => res.render('home'));
+app.get('/', (_, res) => res.render('home'));
+app.get('/*', (_, res) => res.render('/'));
 
+
+// http
 const handleListen = () => console.log('Listening on http://localhost:3000');
+// app.listen(3000, handleListen);
 
-app.listen(3000, handleListen);
+
+// ws
+const server = http.createServer(app);
+const wss = new ws.WebSocketServer({ server });
+
+server.listen(3000, handleListen);
