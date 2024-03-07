e59f7f37ba2a54f639f338b90bbf6f659b5ae544
yyyejinnn <lyjin2007@gmail.com>
2024-03-07 15:06:30 +0900

    ~#1.8: 닉네임 추가하기

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,4 +1,19 @@
+const messageList = document.querySelector('ul');
+const nickForm = document.querySelector('#nick')
+const messageForm = document.querySelector('#message');
+
 const socket = new WebSocket(`ws://${window.location.host}`); // to be, 서버와 연결할 소켓
+
+/**
+ * object로 바로 보내면 편할텐데.. string type으로 보내게 만들어진 이유는?
+ * -> be가 js가 아닌 다른 언어를 사용할 경우도 고려해야함
+ * -> 파라미터에 대한 처리는 백엔드에서 처리할 수 있도록 의존성X
+ */
+function makeMessage(type, payload){
+    const msg = {type, payload};
+
+    return JSON.stringify(msg);
+}
 
 // fe 소켓 연결 이벤트
 socket.addEventListener('open', () => {
@@ -7,16 +22,39 @@ socket.addEventListener('open', () => {
 
 // fe 메세지 감지 이벤트
 socket.addEventListener('message', (message) => {
-    console.log('메세지: ', message.data);
+    // console.log('메세지: ', message.data);
+
+    const li = document.createElement('li');
+    li.innerText = message.data;
+    messageList.append(li);
 })
-ß
+
 // fe 소켓 종료 이벤트
 socket.addEventListener('close', () => {
     console.log('소켓 연결 종료');
 })
 
-setTimeout(()=>{
+function handleSubmit(event){
+    event.preventDefault();
+    const input = messageForm.querySelector('input');
+    socket.send(makeMessage('message', input.value));
+    input.value = '';
 
-    // 이벤트 발생 -> 10번째 줄 message.data
-    socket.send('hello from the browser!');
-}, 1000)
+}
+
+function handleNickSubmit(event){
+    event.preventDefault();
+    const input = nickForm.querySelector('input');
+    socket.send(makeMessage('nickname', input.value));
+    input.value = '';
+}
+
+messageForm.addEventListener('submit', handleSubmit);
+nickForm.addEventListener('submit', handleNickSubmit);
+
+
+//setTimeout(()=>{
+//
+//    // 이벤트 발생 -> 10번째 줄 message.data
+//    socket.send('hello from the browser!');
+//}, 1000)
--- src/server.js
+++ src/server.js
@@ -21,16 +21,45 @@ const handleListen = () => console.log('Listening on http://localhost:3000');
 const server = http.createServer(app);
 const wss = new ws.WebSocketServer({ server });
 
+/**
+ * wss vs. socket
+ * wss.on()은 서버 전체를 위한 소켓 서버 관리
+ * socket.on()은 특정 소켓 단위로 관리
+ */
+
+const sockets = [];
+
 wss.on('connection', (socket) => { // from fe, 연결된 브라우저
     console.log('서버 연결 시작');
 
     // fe로부터 받아온 message 감지
-    socket.on('message', message => console.log(message.toString('utf-8')));
+    // socket.on('message', message => console.log(message.toString('utf-8')));
+    // socket.on('message', (message) => socket.send(message.toString()));
+
+    sockets.push(socket);
+    socket['nickname'] = 'anon';
+
+    socket.on('message', (message) => {
+        const msgObj = JSON.parse(message.toString());
+
+        const msgType = msgObj.type;
+        const msgPayload = msgObj.payload.toString();
+
+        switch(msgType){
+            case 'message': {
+                sockets.forEach(aSocket => {
+                    aSocket.send(`${socket['nickname']}: ${msgPayload}`)
+                });
+                break;
+            }
+            case 'nickname': {
+                socket['nickname'] = msgPayload;
+            }
+        }
+    })
 
     // be 소켓 연결 종료
     socket.on('close', ()=> console.log('서버 연결 종료'));
-
-    // socket.send('hello!!!');s
 })
 
 server.listen(3000, handleListen);
