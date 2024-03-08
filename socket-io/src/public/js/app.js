079715520479c71a0d494a44f1237cddf19bd76a
yyyejinnn <lyjin2007@gmail.com>
2024-03-08 14:24:48 +0900

    ~#2.5: 채팅방 만들기

--- socket-io/src/public/js/app.js
+++ socket-io/src/public/js/app.js
@@ -2,6 +2,18 @@ const socket = io();
 
 const welcome = document.getElementById('welcome');
 const form = document.querySelector('form');
+const room = document.getElementById('room');
+
+room.hidden = true;
+
+let roomName;
+
+function showRoom(){
+    welcome.hidden = true;
+    room.hidden = false;
+    const h3 = room.querySelector('h3');
+    h3.innerText = `Room ${roomName}`;
+}
 
 function handleRoomSubmit(event){
     event.preventDefault();
@@ -14,9 +26,25 @@ function handleRoomSubmit(event){
      * 콜백 함수도 전달 가능
      * -> 가장 마지막에 위치해야함
      * -> be에서 실행되는게 아닌 be으로부터 fe함수를 실행시키는 것
+     * 
      */
-    socket.emit('enter_room', { payload: input.value }, () => console.log('server is done!'));
+
+    roomName = input.value;
+
+    socket.emit('enter_room', roomName, showRoom);
     input.value = '';
 }
 
 form.addEventListener('submit', handleRoomSubmit);
+
+/** */
+function addMessage(msg){
+    const ul = room.querySelector('ul');
+    const li = document.createElement('li');
+    li.innerText = msg;
+    ul.appendChild(li);
+}
+
+socket.on('welcome', () => {
+    addMessage('Someone joined!');
+})
--- socket-io/src/server.js
+++ socket-io/src/server.js
@@ -9,17 +9,22 @@ app.set('views', __dirname + '/views');
 app.use('/public', express.static(__dirname + '/public'));
 
 app.get('/', (_, res) => res.render('home'));
-app.get('/*', (_, res) => res.render('/'));
-
+// app.get('/*', (_, res) => res.render('/'));
 
 // socketIO
 const server = http.createServer(app);
 const ioServer = socketIO(server);
 
 ioServer.on('connection', socket => {
-    socket.on('enter_room', (msg, fn) => {
-        console.log(msg);    // { payload: <value> }
-        setTimeout(() => fn(), 3000);
+    /**
+     * socket.join()
+     * 여기서 room이란 채팅에 국한되지않음
+     * -> 배민 주문을 하면 배달원과 나 사이의 'room'이 그룹핑 되는 것
+     */
+    socket.on('enter_room', (roomName, fn) => {
+        socket.join(roomName);
+        fn();
+        socket.to(roomName).emit('welcome');    // 나를 제외한 방 안의 모든 이에게 이벤트 적용
     })
 });
 
